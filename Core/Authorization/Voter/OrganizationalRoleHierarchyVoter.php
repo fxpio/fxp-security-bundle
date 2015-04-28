f4dc92d8306ab07755e1716ed6f0fc03b217d5b7
François Pluchino <francois.pluchino@sonatra.com>
2015-04-28 19:22:37 +0200

    Override the role hierarchy voter for organizational context

---
+++ Core/Authorization/Voter/OrganizationalRoleHierarchyVoter.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\SecurityBundle\Core\Authorization\Voter;
+
+use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
+use Symfony\Component\Security\Core\Authorization\Voter\RoleVoter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Acl\Model\SecurityIdentityRetrievalStrategyInterface;
+use Symfony\Component\Security\Core\Role\Role;
+
+/**
+ * OrganizationalRoleHierarchyVoter uses a RoleHierarchy and organizational context to
+ * determine the roles granted to the user before voting.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class OrganizationalRoleHierarchyVoter extends RoleVoter
+{
+    /**
+     * @var SecurityIdentityRetrievalStrategyInterface
+     */
+    private $sidRetrievalStrategy;
+
+    /**
+     * Constructor.
+     *
+     * @param SecurityIdentityRetrievalStrategyInterface $sidRetrievalStrategy
+     * @param string                                     $prefix
+     */
+    public function __construct(SecurityIdentityRetrievalStrategyInterface $sidRetrievalStrategy, $prefix = 'ROLE_')
+    {
+        $this->sidRetrievalStrategy = $sidRetrievalStrategy;
+        parent::__construct($prefix);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function extractRoles(TokenInterface $token)
+    {
+        $sids = $this->sidRetrievalStrategy->getSecurityIdentities($token);
+        $roles = array();
+
+        foreach ($sids as $sid) {
+            if ($sid instanceof RoleSecurityIdentity) {
+                $roles[] = new Role($sid->getRole());
+            }
+        }
+        var_dump($roles);
+
+        return $roles;
+    }
+}
